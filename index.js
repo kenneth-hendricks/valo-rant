5a28e60ac7ba17acb2f3909a35f895ad9a1a94e9
Kenneth Hendricks <kennethhendricks@catalyst-au.net>
2020-08-27 20:16:19 +1000

    Add beginnings of !soundthehorn command

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("message", (message) => {
-  if (message.content === "!doesthiswork") {
-    message.channel.send("yes");
+const valorantRoleId = "748474101479374919";
+const hornUrl =
+  "https://media1.tenor.com/images/dfe647c8913c3b5ae373bc84d894c3e8/tenor.gif?itemid=8651046";
+
+client.on("message", async (message) => {
+  if (message.content === "!soundthehorn") {
+    const valorantMembers = (await message.guild.members.fetch()).filter(
+      (member) => {
+        return member._roles.includes(valorantRoleId);
+      }
+    );
+
+    const messageContent = valorantMembers
+      .map((member) => {
+        return `<@${member.user.id}>`;
+      })
+      .join(" ");
+
+    message.channel.send(messageContent, { files: [hornUrl] });
   }
 });
 
